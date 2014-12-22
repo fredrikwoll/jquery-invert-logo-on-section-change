5c73e56ef888eff400f7382f5495598adc7ccc37
fredrikwoll <kontakt@fredrikwoll.no>
2014-12-22 18:04:34 +0100

    crossbrowser issues
    Fixed a bug where the scrolltop value wouldn't be registered in firefox.

--- assets/js/logoinvert.jquery.js
+++ assets/js/logoinvert.jquery.js
@@ -12,25 +12,45 @@ $(document).ready(function () {
   $('.logo2').hide();
   
 });
+
+
+function getScrollTop(){
+    if(typeof pageYOffset!= 'undefined'){
+        //most browsers except IE before #9
+        return pageYOffset;
+    }
+    else{
+        var B= document.body; //IE 'quirks'
+        var D= document.documentElement; //IE with doctype
+        D= (D.clientHeight)? D: B;
+        return D.scrollTop;
+    }
+}
+
+
 $(document).scroll(function (e) {
 
       var $valueSectionOne = $('#section1').offset().top;
       var $valueSectionTwo = $('#section2').offset().top;
       var $valueSectionTree = $('#section3').offset().top;
-      var docScroll = document.body.scrollTop;
+      var docScroll = getScrollTop();
 
 
        if(docScroll > 0 && docScroll <= $valueSectionTwo){
           $('.logo2').hide();
           $('.logo1').show();
+          return false;
+
         }
         if(docScroll >= $valueSectionTwo && docScroll <= $valueSectionTree){
            $('.logo1').hide();  
            $('.logo2').show();
+          return false;
         }
         if(docScroll > $valueSectionTree){
            $('.logo2').hide();  
            $('.logo1').show();
+          return false;           
         }
    			
   
